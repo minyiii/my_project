2f26ec999935048fca20abca50984eed32dcf5fe
minyiii <25.7minyi@gmail.com>
2020-09-26 23:14:57 +0800

    多import csrf exempt

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from django.views.generic import TemplateView # 新增的
+from django.views.decorators.csrf import csrf_exempt
 
 
 
